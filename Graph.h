bbb09671cea7f41d8ab0a297a0ee59285ffa56c1
drussell23 <djamesr23@gmail.com>
2018-05-28 23:21:47 -0700

    Add files via upload

--- Graph.h
+++ Graph.h
@@ -166,3 +166,143 @@ class Graph
 };
 #endif // GRAPH_H
 #pragma once
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+#include <cstdlib>
+#include <locale>
+
+using namespace std;
+
+struct Graph_MAP
+{
+	string Location;
+	vector<string> The_List_Of_Cities;
+};
+class Graph
+{
+	public:
+		Graph_MAP MyList[300];
+		int Count_City;
+
+	public:
+		Graph()
+		{
+			Count_City = 0;
+		}
+		void ADDCity_start(string N)
+		{
+			MyList[Count_City++].Location = N;
+		}
+		void AddToCity_end(int n, string A)
+		{
+			MyList[Count_City].The_List_Of_Cities.push_back(A);
+		}
+		int FindCity_find(string City)
+		{
+			vector<int> List;
+			int a = 0;
+
+			for (int count = 0; count < Count_City; count++)
+			{
+				if (MyList[count].Location.find(City) != -1)
+				{
+					List.push_back(count);
+					cout << a << ". " << MyList[count].Location << endl;
+					a++;
+				}
+			}
+			cout << endl;
+
+			if (a > 0)
+			{
+				int decision = -1;
+
+				while (decision < 0 || decision > a)
+				{
+					cout << "Make a decision: (0-" << a - 1 << "): ";
+					cin >> decision;
+				}
+				cout << endl;
+				return(List[decision]);
+			}
+			else
+			{
+				return (-1);
+			}
+		}
+		void Flight(int Flight_Route, int T)
+		{
+			cout << "Now leaving: " << MyList[Flight_Route].Location << " going to: " << MyList[T].Location << endl;
+
+			int a[300];
+
+			for (int i = 0; i < 300; i++)
+			{
+				a[i] = -1;
+			}
+			string b[300];
+			a[Flight_Route] = Flight_Route;
+			b[0] = MyList[Flight_Route].Location;
+			int y = 0, c = 0;
+
+			while (y >= c)
+			{
+				int point = 0;
+
+				for (int i = 0; i < Count_City; i++)
+				{
+					if (b[c] == MyList[i].Location)
+					{
+						point = i;
+					}
+				}
+				for (unsigned int i = 0; i < MyList[point].The_List_Of_Cities.size(); i++)
+				{
+					string q = MyList[point].The_List_Of_Cities[i];
+
+					for (int j = 0; j < Count_City; j++)
+					{
+						if (MyList[j].Location == q && a[j] == -1)
+						{
+							y++;
+							b[y] = q;
+							a[j] = point;
+						}
+					}
+				}
+				c++;
+			}
+			if(a[y] == -1)
+			{
+				cout << "Restricted Flight." << endl;
+			}
+			else
+			{
+				vector<string> r;
+				r.push_back(MyList[T].Location);
+				int e = a[y];
+
+				while (e != Flight_Route)
+				{
+					r.push_back(MyList[e].Location);
+					e = a[e];
+				}
+				r.push_back(MyList[Flight_Route].Location);
+
+				for (int i = r.size() - 1; i >= 0; i--)
+				{
+					cout << r[i] << endl;
+				}
+				cout << endl;
+			}
+		}
+};
+#endif // GRAPH_H
+#pragma once
